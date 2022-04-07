d30ba5d36d152293b9365bd0e1ebf107b9468bc7
Nicodemus <nicodemusdu@gmail.com>
2022-04-07 11:08:11 +0800

    React JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+const welcome = {
+    greeting: "Hey",
+    title: "React",
+};
+function getTitle(title) {
+    return title;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = "React LL";
+    return (
+        <div>
+            <h1>
+                {welcome.greeting} {welcome.title} {getTitle("dudu")}
+            </h1>
+            <label htmlfor="search">Search</label>
+            <input id="search" type="text" />
+        </div>
+    );
 }
 
 export default App;
