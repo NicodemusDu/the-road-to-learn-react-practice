fe3eb92c6a3c304e05084049c26f15b64874a2af
Nicodemus <nicodemusdu@gmail.com>
2022-04-07 23:31:35 +0800

    JSX 中的内联处理函数

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const stories = [
+    const initialStories = [
         {
             title: 'React',
             url: 'https://reactjs.org/',
@@ -31,21 +31,40 @@ const App = () => {
         }, [value, key]);
         return [value, setValue];
     };
-    const [searchTerm, setSearchTerm] = useSemiPersistentState(
-        'search',
-        'React'
-    );
+    const [searchTerm, setSearchTerm] = useSemiPersistentState('search', '');
 
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
         console.log('handleSearch: ', event.target.value);
     };
 
-    const searchStories = stories.filter(function (story) {
-        console.log('searchStories: ', story.title);
-        return story.title.toLowerCase().includes(searchTerm.toLowerCase());
-    });
+    // const searchStories = initialStories.filter(function (story) {
+    //     console.log('searchStories: ', story.title);
+    //     return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+    // });
 
+    const [stores, setStores] = React.useState(initialStories);
+
+    const handleRemoveStory = (item) => {
+        const newStories = stores.filter(
+            (story) => item.objectID !== story.objectID
+        );
+        setStores(newStories);
+        console.log('newStories: ', newStories);
+    };
+
+    const [searchStories, setSearchStories] = React.useState(initialStories);
+
+    React.useEffect(() => {
+        setSearchStories(
+            stores.filter(function (story) {
+                console.log('searchStories: ', story.title);
+                return story.title
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase());
+            })
+        );
+    }, [stores, searchTerm]);
     return (
         <>
             <h1>My Hacker Stories</h1>
@@ -58,22 +77,32 @@ const App = () => {
                 Search
             </InputWithLabel>
             <hr />
-            <List list={searchStories} />
+            <List list={searchStories} onRemoveItem={handleRemoveStory} />
         </>
     );
 };
-const List = ({ list }) =>
-    list.map((item) => <Item key={item.objectID} item={item} />);
-const Item = ({ item }) => (
-    <>
-        <span>
-            <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-    </>
-);
+const List = ({ list, onRemoveItem }) =>
+    list.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+    ));
+
+const Item = ({ item, onRemoveItem }) => {
+    return (
+        <div>
+            <span>
+                <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author} </span>
+            <span> {item.num_comments} </span>
+            <span> {item.points} </span>
+            <span>
+                <button type="button" onClick={() => onRemoveItem(item)}>
+                    Dismiss
+                </button>
+            </span>
+        </div>
+    );
+};
 const InputWithLabel = ({
     id,
     value,
