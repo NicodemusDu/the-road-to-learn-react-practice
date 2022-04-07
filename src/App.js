c130a9677af63040302991e80a16edb98dc79e87
Nicodemus <nicodemusdu@gmail.com>
2022-04-07 18:08:56 +0800

    Props 处理（高级）

--- src/App.js
+++ src/App.js
@@ -41,28 +41,24 @@ const App = () => {
         </div>
     );
 };
-const List = (props) =>
-    props.list.map((item) => (
-        <div key={item.objectID}>
-            <span>
-                <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-        </div>
-    ));
-const Search = (props) => {
-    return (
-        <div>
-            <label htmlfor="search">Search</label>
-            <input
-                id="search"
-                type="text"
-                value={props.search}
-                onChange={props.onSearch}
-            />
-        </div>
-    );
-};
+const List = ({ list }) =>
+    list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />);
+
+const Item = ({ title, url, author, num_comments, points }) => (
+    <div>
+        <span>
+            <a href={url}>{title}</a>
+        </span>
+        <span>{author}</span>
+        <span>{num_comments}</span>
+        <span>{points}</span>
+    </div>
+);
+
+const Search = ({ search, onSearch }) => (
+    <div>
+        <label htmlFor="search">Search</label>
+        <input id="search" type="text" value={search} onChange={onSearch} />
+    </div>
+);
 export default App;
