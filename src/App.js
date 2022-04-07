f39bb28a324fb9f077aa948822b7b589c9749e6e
Nicodemus <nicodemusdu@gmail.com>
2022-04-07 15:45:43 +0800

    JSX中的处理函数

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom';
 
 const cun = (count) => count + 1;
 
-function App() {
+const App = () => {
     return (
         <div>
             <h1>My Hacker Stories</h1>
 
             <label htmlfor="search">Search</label>
-            <input id="search" type="text" />
+            {/* <input id="search" type="text" onChange={handleChange} /> */}
+            <input
+                id="search"
+                type="text"
+                onChange={(event) => console.log(event.target.value)}
+            />
 
             <hr />
         </div>
     );
-}
+};
 
 export default App;
