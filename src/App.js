90a141d8985326f94252d0916b698aa5ff73cd51
Nicodemus <nicodemusdu@gmail.com>
2022-04-07 11:40:58 +0800

    React中的列表

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const welcome = {
-    greeting: "Hey",
-    title: "React",
-};
-function getTitle(title) {
-    return title;
-}
+const list = [
+    {
+        title: "React",
+        url: "https://reactjs.org/",
+        author: "Jordan Walke",
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: "Redux",
+        url: "https://redux.js.org/",
+        author: "Dan Abramov, Andrew Clark",
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
 function App() {
-    const title = "React LL";
     return (
         <div>
-            <h1>
-                {welcome.greeting} {welcome.title} {getTitle("dudu")}
-            </h1>
+            <h1>My Hacker Stories</h1>
+
             <label htmlfor="search">Search</label>
             <input id="search" type="text" />
+
+            <hr />
+            <List />
         </div>
     );
 }
 
+function List() {
+    return list.map(function (item) {
+        return (
+            <div key={item.objectIDj}>
+                <span>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                {item.title}
+            </div>
+        );
+    });
+}
 export default App;
