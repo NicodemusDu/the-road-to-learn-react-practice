f08ab034faa8211d380b1e106f4dd422138bd171
Nicodemus <nicodemusdu@gmail.com>
2022-04-07 15:50:49 +0800

    React Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const cun = (count) => count + 1;
-
 const App = () => {
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
     return (
         <div>
             <h1>My Hacker Stories</h1>
@@ -17,8 +33,19 @@ const App = () => {
             />
 
             <hr />
+            <List list={stories} />
         </div>
     );
 };
-
+const List = (props) =>
+    props.list.map((item) => (
+        <div key={item.objectID}>
+            <span>
+                <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+        </div>
+    ));
 export default App;
