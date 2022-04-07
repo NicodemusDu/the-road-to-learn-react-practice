1a76ff24628dbd4a37b39d1ec33dc602982bb4f6
Nicodemus <nicodemusdu@gmail.com>
2022-04-07 15:34:03 +0800

    React函数定义（箭头函数）

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const list = [
-    {
-        title: "React",
-        url: "https://reactjs.org/",
-        author: "Jordan Walke",
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: "Redux",
-        url: "https://redux.js.org/",
-        author: "Dan Abramov, Andrew Clark",
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
+const cun = (count) => count + 1;
+
 function App() {
     return (
         <div>
@@ -28,24 +12,8 @@ function App() {
             <input id="search" type="text" />
 
             <hr />
-            <List />
         </div>
     );
 }
 
-function List() {
-    return list.map(function (item) {
-        return (
-            <div key={item.objectIDj}>
-                <span>
-                    <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                {item.title}
-            </div>
-        );
-    });
-}
 export default App;
