5152bce7be20972aa8b8a855d912a9e93443b337
Nicodemus <nicodemusdu@gmail.com>
2022-04-08 17:29:03 +0800

    React 中的第三方库

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 const useSemiPersistentState = (key, initState) => {
@@ -64,12 +64,12 @@ const App = () => {
     const handleFetchStories = React.useCallback(() => {
         dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-        fetch(url)
-            .then((response) => response.json())
+        axios
+            .get(url)
             .then((result) =>
                 dispatchStories({
                     type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.hits,
+                    payload: result.data.hits,
                 })
             )
             .catch(() =>
