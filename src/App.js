6e65ef92b77fe8bb5841e573f50a6964f9a2a47d
Nicodemus <nicodemusdu@gmail.com>
2022-04-10 17:36:17 +0800

    React 项目结构

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import List from './List';
+import SearchForm from './SearchForm';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -121,75 +123,4 @@ const App = () => {
     );
 };
 
-const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-    <form onSubmit={onSearchSubmit}>
-        <InputWithLabel
-            id="search"
-            value={searchTerm}
-            isFocused
-            onInputChange={onSearchInput}
-        >
-            <strong>Search:</strong>
-        </InputWithLabel>
-
-        <button type="submit" disabled={!searchTerm}>
-            Submit
-        </button>
-    </form>
-);
-
-const InputWithLabel = ({
-    id,
-    value,
-    type = 'text',
-    onInputChange,
-    isFocused,
-    children,
-}) => {
-    const inputRef = React.useRef();
-
-    React.useEffect(() => {
-        if (isFocused && inputRef.current) {
-            inputRef.current.focus();
-        }
-    }, [isFocused]);
-
-    return (
-        <>
-            <label htmlFor={id}>{children}</label>
-            &nbsp;
-            <input
-                ref={inputRef}
-                id={id}
-                type={type}
-                value={value}
-                onChange={onInputChange}
-            />
-        </>
-    );
-};
-
-const List = ({ list, onRemoveItem }) =>
-    list.map((item) => (
-        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-    ));
-
-const Item = ({ item, onRemoveItem }) => (
-    <div>
-        <span>
-            <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-            <button type="button" onClick={() => onRemoveItem(item)}>
-                Dismiss
-            </button>
-        </span>
-    </div>
-);
-
 export default App;
-
-export { SearchForm, InputWithLabel, List, Item };
